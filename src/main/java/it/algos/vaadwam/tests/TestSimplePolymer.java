79da69a958ffc7327990bd17b6f31b435e8ddebf
algos-soft <gac@algos.it>
2020-05-07 15:45:07 +0200

    WamLayout.class

--- src/main/java/it/algos/vaadwam/tests/TestSimplePolymer.java
+++ src/main/java/it/algos/vaadwam/tests/TestSimplePolymer.java
@@ -1,16 +1,16 @@
 package it.algos.vaadwam.tests;
 
 import com.vaadin.flow.component.Tag;
-import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.dependency.HtmlImport;
 import com.vaadin.flow.component.polymertemplate.PolymerTemplate;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.templatemodel.TemplateModel;
+import it.algos.vaadwam.WamLayout;
 
 /**
  * Simple Polymer test
  */
-@Route(value = "simplepolymer", layout = AppLayout.class)
+@Route(value = "simplepolymer", layout = WamLayout.class)
 //@ParentLayout(AppLayout.class)
 @Tag("simple-polymer")
 @HtmlImport("src/views/prova/simple-polymer.html")
