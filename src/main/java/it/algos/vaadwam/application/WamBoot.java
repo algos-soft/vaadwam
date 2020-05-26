35721d5ef92ee084d23b6ea3f8ba38e4c0313d6d
algos-soft <gac@algos.it>
2020-05-26 12:08:59 +0200

    ...

--- src/main/java/it/algos/vaadwam/application/WamBoot.java
+++ src/main/java/it/algos/vaadwam/application/WamBoot.java
@@ -66,7 +66,7 @@ public class WamBoot extends ABoot {
 
     private final static double PROJECT_VERSION = 0.9;
 
-    private final static LocalDate VERSION_DATE = LocalDate.of(2020, 5, 24);
+    private final static LocalDate VERSION_DATE = LocalDate.of(2020, 5, 26);
 
     /**
      * Inietta da Spring come 'singleton'
