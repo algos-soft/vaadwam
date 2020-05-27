60a1f7d3440a48d60f677962abe8a2587610e252
algos-soft <gac@algos.it>
2020-05-27 07:35:02 +0200

    ...

---
+++ src/main/java/it/algos/vaadwam/modules/log/WamLogService.java
@@ -0,0 +1,39 @@
+package it.algos.vaadwam.modules.log;
+
+import com.vaadin.flow.spring.annotation.SpringComponent;
+import it.algos.vaadflow.annotation.AIScript;
+import it.algos.vaadflow.modules.log.LogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import static it.algos.vaadflow.application.FlowCost.TAG_LOG;
+
+/**
+ * Project vaadwam
+ * Created by Algos
+ * User: gac
+ * Date: mer, 27-mag-2020
+ * Time: 07:32
+ */
+@SpringComponent
+@Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
+@AIScript(sovrascrivibile = false)
+public class WamLogService extends LogService {
+
+    /**
+     * Costruttore @Autowired <br>
+     * Si usa un @Qualifier(), per avere la sottoclasse specifica <br>
+     * Si usa una costante statica, per essere sicuri di scrivere sempre uguali i riferimenti <br>
+     * Regola nella superclasse il modello-dati specifico <br>
+     *
+     * @param repository per la persistenza dei dati
+     */
+    @Autowired
+    public WamLogService(@Qualifier(TAG_LOG) MongoRepository repository) {
+        super(repository);
+    }// end of Spring constructor
+
+}
