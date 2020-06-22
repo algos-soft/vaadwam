915e9a3e0996c0310f5e236beccc05159274aae3
algos-soft <gac@algos.it>
2020-06-22 11:19:24 +0200

    fix note con asterisco se import nota vuota

--- src/main/java/it/algos/vaadwam/application/WamBoot.java
+++ src/main/java/it/algos/vaadwam/application/WamBoot.java
@@ -66,7 +66,7 @@ public class WamBoot extends ABoot {
 
     private final static String PROJECT_BANNER = "Gestione Ambulanze";
 
-    private final static double PROJECT_VERSION = 1.4;
+    private final static double PROJECT_VERSION = 1.5;
 
     private final static LocalDate VERSION_DATE = LocalDate.of(2020, 6, 22);
 
