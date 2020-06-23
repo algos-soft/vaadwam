106e1942c29ba079bc1e6c51382e23632367977f
algos-soft <algos12>
2020-06-23 18:19:13 +0200

    1.7

--- src/main/java/it/algos/vaadwam/application/WamBoot.java
+++ src/main/java/it/algos/vaadwam/application/WamBoot.java
@@ -66,7 +66,7 @@ public class WamBoot extends ABoot {
 
     private final static String PROJECT_BANNER = "Gestione Ambulanze";
 
-    private final static double PROJECT_VERSION = 1.6;
+    private final static double PROJECT_VERSION = 1.7;
 
     private final static LocalDate VERSION_DATE = LocalDate.of(2020, 6, 23);
 
